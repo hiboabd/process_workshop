4da72cb0a15630134efa02099fc142e280f7f5f2
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-04 19:00:19 +0100

    Tests pass: returns 2 for aa

--- scrabble_solver/lib/scrabble.rb
+++ scrabble_solver/lib/scrabble.rb
@@ -10,10 +10,11 @@ class Scrabble
     score = 0
     return 0 if @word == " " || @word == "nil"
     word_array = @word.split('')
+    p "word array: #{word_array}"
     word_array.each do |letter|
-      score = 0 if letter == "'\'"
-      score = 1 if letter == "a"
-      score = 4 if letter == "f"
+      score += 0 if letter == "'\'"
+      score += 1 if letter == "a"
+      score += 4 if letter == "f"
     end
     return score
   end
