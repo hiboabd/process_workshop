22e89c3b293505d9a70f4537256f5fc9a8d351cb
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-05 08:39:39 +0100

    changed score variable to instance variable @score

--- scrabble_solver/lib/scrabble.rb
+++ scrabble_solver/lib/scrabble.rb
@@ -1,35 +1,35 @@
 class Scrabble
 
-  attr_reader :word
+  attr_reader :word, :score
 
   def initialize(word)
     @word = word
+    @score = 0
   end
 
   def score
-    score = 0
     return 0 if @word == " " || @word == "nil"
     word_array = @word.split('')
     p "word array: #{word_array}"
     word_array.each do |letter|
       if letter == "'\'"
-        score += 0
+        @score += 0
       elsif letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u" || letter == "l" || letter == "n" || letter == "r" || letter == "s" || letter == "t"
-        score += 1
+        @score += 1
       elsif letter == "d" || letter == "g"
-        score += 2
+        @score += 2
       elsif letter == "b" || letter == "c" || letter == "m" || letter == "p"
-        score += 3
+        @score += 3
       elsif letter == "f" || letter == "h" || letter == "v" || letter == "w" || letter == "y"
-        score += 4
+        @score += 4
       elsif letter == "k"
-        score += 5
+        @score += 5
       elsif letter == "j" || letter == "x"
-        score += 8
+        @score += 8
       elsif letter == "q" || letter == "z"
-        score += 10
+        @score += 10
       end
     end
-    return score
+    return @score
   end
 end
