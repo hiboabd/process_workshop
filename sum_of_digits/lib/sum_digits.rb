2c65fbb05dada5ed02950eb2bb075d1782ec0919
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-06 11:08:34 +0100

    tests pass, returnSum() iterates over digits and sums

--- sum_of_digits/lib/sum_digits.rb
+++ sum_of_digits/lib/sum_digits.rb
@@ -4,7 +4,20 @@ class SumDigits
   end
 
   def returnSum
-    return 7 if @number == 16
-    return 6 if @number == 942
+    sum = 0
+    digit_array = @number.to_s.split('')
+
+    digit_array.each do |digit|
+      sum += digit.to_i
+    end
+
+    if sum.to_s.split('').length > 1
+      digit_array = sum.to_s.split('')
+      sum = 0
+      digit_array.each do |digit|
+        sum += digit.to_i
+      end
+    end
+    return sum
   end
 end
