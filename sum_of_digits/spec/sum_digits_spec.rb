8186384549bbaea9ea65f6ae74fe405230f3375d
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-06 10:50:56 +0100

    tests failing for number 942

--- sum_of_digits/spec/sum_digits_spec.rb
+++ sum_of_digits/spec/sum_digits_spec.rb
@@ -7,6 +7,11 @@ describe SumDigits do
        sumDigits = SumDigits.new(16)
        expect(sumDigits.returnSum()).to eq(7)
      end
+
+     it 'returns 6 for 942' do
+       sumDigits = SumDigits.new(942)
+       expect(sumDigits.returnSum()).to eq(6)
+     end
   end
 
 end
