8ef1cdd8b0913bc6b1c83015ce276f51545b7405
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-06 12:50:47 +0100

    refactored method

--- narcisstic_number/lib/narcissistic.rb
+++ narcisstic_number/lib/narcissistic.rb
@@ -1,18 +1,15 @@
 class Narcissistic
   def initialize
+    @sum = 0
   end
 
   def narcissistic?(num)
-    sum = 0
     digit_array = num.to_s.split('')
+
     digit_array.each do |digit|
-      sum += (digit.to_i ** digit_array.length)
-    end
-    if sum == num
-      return true
-    else
-      return false
+      @sum += (digit.to_i ** digit_array.length)
     end
 
+    @sum == num ? true : false 
   end
 end
