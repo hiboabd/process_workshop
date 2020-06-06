397e739d01f583fce46a9b14c18be70a0df92b86
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-06 12:48:31 +0100

    Added all tests; all pass

--- narcisstic_number/spec/narcissistic_spec.rb
+++ narcisstic_number/spec/narcissistic_spec.rb
@@ -1,10 +1,27 @@
 require './lib/narcissistic'
 
 describe Narcissistic do
+  let(:narcisstic) {Narcissistic.new()}
+
   describe '#narcissistic?' do
     it 'returns true for 5' do
-      narcisstic = Narcissistic.new()
       expect(narcisstic.narcissistic?(5)).to eq(true)
+    end
+
+    it 'returns true for 153' do
+      expect(narcisstic.narcissistic?(153)).to eq(true)
+    end
+
+    it 'returns true for 1634' do
+      expect(narcisstic.narcissistic?(1634)).to eq(true)
+    end
+
+    it 'returns true for 1633' do
+      expect(narcisstic.narcissistic?(1633)).to eq(false)
+    end
+
+    it 'returns true for 152' do
+      expect(narcisstic.narcissistic?(152)).to eq(false)
     end
   end
 end
