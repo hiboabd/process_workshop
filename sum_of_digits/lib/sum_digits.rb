5194f631de854447586b2e80522deab2f813a3eb
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-06 11:12:37 +0100

    refactored returnSum method, new calculateSum method and instance variables

--- sum_of_digits/lib/sum_digits.rb
+++ sum_of_digits/lib/sum_digits.rb
@@ -1,23 +1,24 @@
 class SumDigits
   def initialize(num)
     @number = num
+    @sum = 0
+    @digit_array = @number.to_s.split('')
   end
 
   def returnSum
-    sum = 0
-    digit_array = @number.to_s.split('')
+    calculateSum()
 
-    digit_array.each do |digit|
-      sum += digit.to_i
+    if @sum.to_s.split('').length > 1
+      @digit_array = @sum.to_s.split('')
+      @sum = 0
+      calculateSum()
     end
+    return @sum
+  end
 
-    if sum.to_s.split('').length > 1
-      digit_array = sum.to_s.split('')
-      sum = 0
-      digit_array.each do |digit|
-        sum += digit.to_i
-      end
+  def calculateSum
+    @digit_array.each do |digit|
+      @sum += digit.to_i
     end
-    return sum
   end
 end
