857b21b467cd822788058e01128c91870362facc
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 17:07:02 +0100

    failing test: shop checkout method should return 30 for B

--- checkout/spec/ShopSpec.js
+++ checkout/spec/ShopSpec.js
@@ -8,7 +8,11 @@ describe('Shop', () => {
 
   describe('Checkout method', () => {
     it('returns 50 for A', () => {
-      expect(shop.checkout('A')).toEqual(50)
+      expect(shop.checkout('A')).toEqual(50);
+    });
+
+    it('returns 30 for B', () => {
+      expect(shop.checkout('B')).toEqual(30);
     });
   });
 });
