164e850a6515790b67b42a7987063a23391e3e60
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-15 17:12:19 +0100

    tests pass: returns 15 for D

--- checkout/src/Shop.js
+++ checkout/src/Shop.js
@@ -7,8 +7,10 @@ class Shop {
     if(item === 'A'){
       return 50; } else if (item === 'B'){
         return 30;
-      } else {
+      } else if (item === 'C') {
         return 20;
+      } else {
+        return 15;
       }
   }
 }
